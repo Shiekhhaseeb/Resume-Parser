06ec5d662cc6737ca32f86b8fce9336791df14b2
Shiekhhaseeb <shiekhhaseeb2019@gmail.com>
2025-06-30 09:07:17 +0530

    added upload endpoint, requirements file etc.

--- main.py
+++ main.py
@@ -48,13 +48,14 @@ async def upload_file(file: UploadFile = File(...)):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
 
 

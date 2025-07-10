c6aa0a56cc993d0f3b8db642889623f451563e29
Shiekhhaseeb <shiekhhaseeb2019@gmail.com>
2025-07-10 10:55:34 +0530

    removed unused code /endpoints

--- main.py
+++ main.py
@@ -51,11 +51,6 @@ async def upload_file(file: UploadFile = File(...)):
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {"version": "0.0.1"}
 
 
